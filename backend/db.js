61f2d3370c8434e993a3be213c3dbc2058e64646
gitmravi1104 <147797833+gitmravi1104@users.noreply.github.com>
2024-08-14 13:23:25 +0530

    connecting to public DB

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,11 @@
+
 const mysql = require('mysql');
 const env = require('dotenv');
 
 env.config();
-
+const urlDB=`mysql -hmonorail.proxy.rlwy.net -uroot -pvxnTTnThHwEOWndmHPvVqPsCvjneYHHE --port 12399 --protocol=TCP railway`
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'job_portal',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+urlDB
 });
 
 module.exports = pool;
