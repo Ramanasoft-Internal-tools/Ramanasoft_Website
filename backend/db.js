6e3d9a337acc5409f780705a571f608d94222267
naveensathya7 <110328622+naveensathya7@users.noreply.github.com>
2024-08-14 14:54:41 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,34 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const env = require('dotenv');
 
+// Load environment variables from a .env file (if you're using one)
 env.config();
 
+// Connection string from Railway
+const connectionString = process.env.MYSQL_CONNECTION_STRING  ;
+
+// Parse the connection string
+const url = require('url');
+const connectionParams = url.parse(connectionString);
+const [username, password] = connectionParams.auth.split(':');
+
+// Create a connection pool
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'job_portal',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  host: connectionParams.hostname,
+  port: connectionParams.port,
+  user: username,
+  password: password,
+  database: connectionParams.pathname.split('/')[1]
+});
+
+// Test the connection
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.message);
+  } else {
+    console.log('Connected to the MySQL database');
+    connection.release(); // Release the connection back to the pool
+  }
 });
 
 module.exports = pool;
